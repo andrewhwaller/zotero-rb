4307f72db6b7f182b951d24a9f9733edf4e9d5d3
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-09-01 12:58:25 -0500

    Add write operations to Library class
    Implement create, update, and delete methods for both items and collections
    with support for version tracking, write tokens, and batch operations.
    Includes helper methods to reduce code duplication between item and
    collection operations.

--- lib/zotero/library.rb
+++ lib/zotero/library.rb
@@ -27,9 +27,58 @@ module Zotero
       @client.get("#{@base_path}/tags", params: params)
     end
 
+    def create_item(item_data, version: nil, write_token: nil)
+      create_single("items", item_data, version: version, write_token: write_token)
+    end
+
+    def create_items(items_array, version: nil, write_token: nil)
+      create_multiple("items", items_array, version: version, write_token: write_token)
+    end
+
+    def update_item(item_key, item_data, version: nil)
+      @client.patch("#{@base_path}/items/#{item_key}", data: item_data, version: version)
+    end
+
+    def delete_item(item_key, version: nil)
+      @client.delete("#{@base_path}/items/#{item_key}", version: version)
+    end
+
+    def delete_items(item_keys, version: nil)
+      @client.delete("#{@base_path}/items", version: version, params: { itemKey: item_keys.join(",") })
+    end
+
+    def create_collection(collection_data, version: nil, write_token: nil)
+      create_single("collections", collection_data, version: version, write_token: write_token)
+    end
+
+    def create_collections(collections_array, version: nil, write_token: nil)
+      create_multiple("collections", collections_array, version: version, write_token: write_token)
+    end
+
+    def update_collection(collection_key, collection_data, version: nil)
+      @client.patch("#{@base_path}/collections/#{collection_key}", data: collection_data, version: version)
+    end
+
+    def delete_collection(collection_key, version: nil)
+      @client.delete("#{@base_path}/collections/#{collection_key}", version: version)
+    end
+
+    def delete_collections(collection_keys, version: nil)
+      @client.delete("#{@base_path}/collections", version: version,
+                                                  params: { collectionKey: collection_keys.join(",") })
+    end
+
     private
 
     attr_reader :client, :type, :id, :base_path
+
+    def create_single(resource, data, version: nil, write_token: nil)
+      @client.post("#{@base_path}/#{resource}", data: [data], version: version, write_token: write_token)
+    end
+
+    def create_multiple(resource, data_array, version: nil, write_token: nil)
+      @client.post("#{@base_path}/#{resource}", data: data_array, version: version, write_token: write_token)
+    end
 
     def validate_type(type)
       type_str = type.to_s

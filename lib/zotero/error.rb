f2c370ca3943d1baa04458563b265f918f30c805
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-09-01 12:25:56 -0500

    Add specific error classes for different HTTP status codes

--- lib/zotero/error.rb
+++ lib/zotero/error.rb
@@ -2,4 +2,9 @@
 
 module Zotero
   class Error < StandardError; end
+  class AuthenticationError < Error; end
+  class RateLimitError < Error; end
+  class NotFoundError < Error; end
+  class BadRequestError < Error; end
+  class ServerError < Error; end
 end

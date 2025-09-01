384922bc51dbcf8dd677235c6b28e32a2460341e
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-09-01 11:54:33 -0500

    Remove premature error classes
    - Keep only base Zotero::Error class
    - Remove AuthenticationError, RateLimitError, ValidationError, NotFoundError, ServerError
    - Can add specific errors back when actually needed

--- lib/zotero/error.rb
+++ lib/zotero/error.rb
@@ -2,10 +2,4 @@
 
 module Zotero
   class Error < StandardError; end
-
-  class AuthenticationError < Error; end
-  class RateLimitError < Error; end
-  class ValidationError < Error; end
-  class NotFoundError < Error; end
-  class ServerError < Error; end
 end

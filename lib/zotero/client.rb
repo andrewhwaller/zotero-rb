b85ac7ed11b3b0853b445a8f009c4f43dc622f79
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-09-01 12:27:36 -0500

    Refactor error handling with specific error classes and response format support

--- lib/zotero/client.rb
+++ lib/zotero/client.rb
@@ -40,16 +40,46 @@ module Zotero
       { "Zotero-API-Version" => "3" }
     end
 
-    def handle_response(response)
+    def handle_response(response, format = nil)
+      return parse_response_body(response, format) if response.code.between?(200, 299)
+
+      raise_error_for_status(response)
+    end
+
+    def raise_error_for_status(response)
       case response.code
-      when 200..299
-        response.parsed_response
-      when 401
-        raise Error, "Authentication failed - check your API key"
-      when 404
-        raise Error, "Resource not found: #{response.request.path}"
+      when 400 then raise BadRequestError, "Bad request: #{response.body}"
+      when 401, 403 then raise AuthenticationError, "Authentication failed - check your API key"
+      when 404 then raise NotFoundError, "Resource not found: #{response.request.path}"
+      when 429 then raise_rate_limit_error(response)
+      else raise_server_or_unknown_error(response)
+      end
+    end
+
+    def raise_rate_limit_error(response)
+      backoff = response.headers["backoff"]&.to_i
+      retry_after = response.headers["retry-after"]&.to_i
+      message = "Rate limited."
+      message += " Backoff: #{backoff}s" if backoff
+      message += " Retry after: #{retry_after}s" if retry_after
+      raise RateLimitError, message
+    end
+
+    def raise_server_or_unknown_error(response)
+      case response.code
+      when 500..599
+        raise ServerError, "Server error: HTTP #{response.code} - #{response.message}"
       else
-        raise Error, "HTTP #{response.code}: #{response.message}"
+        raise Error, "Unexpected response: HTTP #{response.code} - #{response.message}"
+      end
+    end
+
+    def parse_response_body(response, format)
+      case format&.to_s
+      when "json", nil
+        response.parsed_response
+      else
+        response.body
       end
     end
   end

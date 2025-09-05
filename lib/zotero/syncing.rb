9f900321f7a34e9709bf73e88434713c9253d5c7
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-09-05 09:35:11 -0500

    docs: add comprehensive documentation to all public methods
    Add detailed @param and @return documentation with type information
    and descriptions for all public methods across Client and module classes:
    
    - Client: initialize, user_library, group_library, make_get_request, make_write_request
    - Fields: item_fields, creator_fields
    - ItemTypes: item_types, item_type_fields, item_type_creator_types, new_item_template
    - Fulltext: fulltext_since, item_fulltext, set_item_fulltext
    - FileAttachments: create_attachment, get_file_info, upload_file, update_file
    - Syncing: verify_api_key, user_groups, deleted_items
    
    All public methods now have comprehensive documentation following
    standard Ruby documentation conventions with parameter types,
    descriptions, and return value information.

--- lib/zotero/syncing.rb
+++ lib/zotero/syncing.rb
@@ -1,19 +1,40 @@
 # frozen_string_literal: true
 
 module Zotero
+  # Syncing and API key verification methods
   module Syncing
+    # Verify that the current API key is valid.
+    #
+    # @return [Hash] API key information including userID and username
     def verify_api_key
-      @client ? @client.get("/keys/current") : get("/keys/current")
+      if @client
+        @client.make_get_request("/keys/current")
+      else
+        make_get_request("/keys/current")
+      end
     end
 
+    # Get groups for a specific user.
+    #
+    # @param user_id [Integer, String] The user ID to get groups for
+    # @param format [String] Response format ('versions' or 'json')
+    # @return [Hash, Array] Groups data in the requested format
     def user_groups(user_id, format: "versions")
-      client = @client || self
-      client.get("/users/#{user_id}/groups", params: { format: format })
+      params = { format: format }
+      if @client
+        @client.make_get_request("/users/#{user_id}/groups", params: params)
+      else
+        make_get_request("/users/#{user_id}/groups", params: params)
+      end
     end
 
+    # Get items that have been deleted from this library.
+    #
+    # @param since [Integer] Optional version to get deletions since
+    # @return [Hash] Object with deleted collections and items arrays
     def deleted_items(since: nil)
       params = since ? { since: since } : {}
-      @client.get("#{@base_path}/deleted", params: params)
+      @client.make_get_request("#{@base_path}/deleted", params: params)
     end
   end
 end
